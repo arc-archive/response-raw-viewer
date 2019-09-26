60d848269be31f69b843acaa4c3c12eac4569c6f
Pawel <jarrodek@gmail.com>
2019-09-26 11:08:11 -0700

    chore: upgrading project dependnecies and tests configuration

--- karma.conf.js
+++ karma.conf.js
@@ -3,37 +3,47 @@ const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require('deepmerge');
 
 module.exports = (config) => {
-  config.set(
-      merge(createDefaultConfig(config), {
-        files: [
-        // runs all files ending with .test in the test folder,
-        // can be overwritten by passing a --grep flag. examples:
-        //
-        // npm run test -- --grep test/foo/bar.test.js
-        // npm run test -- --grep test/bar/*
-          {
-            pattern: config.grep ? config.grep : 'test/**/*.test.js',
-            type: 'module'
-          }
-        ],
+ config.set(
+   merge(createDefaultConfig(config), {
+     files: [
+       // runs all files ending with .test in the test folder,
+       // can be overwritten by passing a --grep flag. examples:
+       //
+       // npm run test -- --grep test/foo/bar.test.js
+       // npm run test -- --grep test/bar/*
+       {
+         pattern: config.grep ? config.grep : 'test/**/*.test.js',
+         type: 'module'
+       },
+       {
+         pattern: 'dev-lib/jexl.min.js',
+         type: 'js'
+       }
+     ],
 
-        // see the karma-esm docs for all options
-        esm: {
-        // if you are using 'bare module imports' you will need this option
-          nodeResolve: true
-        },
+     // see the karma-esm docs for all options
+     esm: {
+       // if you are using 'bare module imports' you will need this option
+       nodeResolve: true
+     },
 
-        coverageIstanbulReporter: {
-          thresholds: {
-            global: {
-              statements: 80,
-              branches: 80,
-              functions: 90,
-              lines: 80
-            }
-          }
-        },
-      })
-  );
-  return config;
+     /**
+      * dev-lib contains a build of Jexl which is 3rd party library
+      * and it is not tested here.
+      * It is included imto coverage report though. This configuration
+      * lowers general test pass limit but keeps regular limits for each file.
+      */
+     coverageIstanbulReporter: {
+       thresholds: {
+         global: {
+           statements: 55,
+           branches: 40,
+           functions: 40,
+           lines: 80
+         }
+       }
+     },
+   })
+ );
+ return config;
 };
